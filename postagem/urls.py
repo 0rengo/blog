308c75f338382fc5b1f7c598c209182f10f60c5c
Orengo <g.orengo@gmail.com>
2017-01-04 16:32:03 -0200

    add segunda pagina, primeira pagina, refatoracao

--- postagem/urls.py
+++ postagem/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import teste
+from .views import primeira_pagina, segunda_pagina
 
 urlpatterns = [
-	url(r'^$', teste, name='teste')
+	url(r'^$', primeira_pagina, name='primeira_pagina'),
+	url(r'^segunda-pagina$', segunda_pagina, name='segunda_pagina')
 ]
--- postagem/views.py
+++ postagem/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-def teste(request):
-	return render(request, 'postagem/teste.html')
+def primeira_pagina(request):
+	nome = 'Gilberto Orengo'
+	metodo = request.method
+	alunos = []
+	# alunos = [
+	# 	'Ana',
+	# 	'Pedro',
+	# 	'Maria'
+	# 	]
+	return render(request, 'postagem/primeira_pagina.html',{'nome': nome, 'metodo': metodo, 'alunos': alunos})
+
+def segunda_pagina(request):
+	return render(request, 'postagem/segunda_pagina.html')
